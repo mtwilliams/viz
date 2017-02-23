f80d607d8e747a1ca9a19b5b1ce6a55c80b89df1
Michael Williams <m.t.williams@live.com>
2017-02-22 21:35:25 -0800

    Reduce verbosity of logging.

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :logger,
   level: :debug,
   utc_log: true,
-  handle_otp_reports: true,
-  handle_sasl_reports: true
+  handle_otp_reports: false,
+  handle_sasl_reports: false
 
 import_config "config.secrets.exs"
